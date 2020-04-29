e9bc5ce5a07a21a812e56d654faa6c3aa4723e03
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-04-29 14:14:35 +0530

    Update linked_list2.c

--- linked_list2.c
+++ linked_list2.c
@@ -13,38 +13,24 @@ int main(){
 	int val;
 	for (val = 1; val<11; val++){
 		nextnode = malloc(sizeof(struct link));
-		printf("\t%d\n", nextnode);
 		nextnode->value = val;
-		printf("\t%d\n", nextnode->value);
-		printf("\t%d\n", start);
 		if (start == NULL) {
 			nextnode->next = NULL;
-			printf("\t%d in if condition..\n", nextnode->next);
 			start = nextnode;
-			printf("\t%d in if condition..\n", start);
 		}
 		else {
 			temp = start;
-			printf("\t%d in else condition..\n", temp);
 			while (temp->next != NULL){
-				printf("\t%din else/while loop..\n", temp->next);
 				temp = temp->next;
-				printf("\t%din else/while loop..\n", temp);
 			}
 			temp->next = nextnode;
-			printf("\t%din else condition..\n", temp->next);
 			nextnode->next = NULL;
-			printf("\t%din else condition..\n", nextnode->next);
 		}
 	}
-	
 	temp = start;
-	printf("\t%d\n", temp);
 	while (temp != NULL){
 		printf("%d\n",temp->value);
 		temp = temp->next;
-		printf("\t%d\n", temp);
 	}
-	
 	return 0;
 }

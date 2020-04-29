1031d6d598ff4c18e08b3923ede2598c5a8dcd18
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-04-30 00:45:05 +0530

    Updated LinkedList3.c

---
+++ linked_list3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct link{
+	int value;
+	struct link *next;
+};
+
+struct link * start = NULL;
+
+int main(){
+	struct link *temp, *nextnode;
+	int val;
+	for (val = 1; val<11; val++){
+		nextnode = malloc(sizeof(struct link));
+		nextnode->value = 5 * val;
+		if (start == NULL) {
+			nextnode->next = NULL;
+			start = nextnode;
+		}
+		else {
+			temp = start;
+			while (temp->next != NULL){
+				temp = temp->next;
+			}
+			temp->next = nextnode;
+			nextnode->next = NULL;
+		}
+	}
+	
+	temp = start;
+	while (temp != NULL){
+		printf("%d\n", temp->value);
+		temp = temp->next;
+	}
+	
+	return 0;
+}

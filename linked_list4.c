c988a56ecceaed2812b869c0240266688e0e9729
Tejas A. Sheth <52593382+TejasSheth104@users.noreply.github.com>
2020-04-30 19:43:08 +0530

    Add files via upload

---
+++ linked_list4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct link{
+	char name[100];
+	struct link *next;
+};
+
+struct link * start = NULL;
+
+int main(){
+	struct link *temp, *nextnode;
+	char Name[100], letter[1];
+	int i = 0;
+	printf("Enter your Name - ");
+	scanf("%s", &Name);
+	while (Name[i] != '\0'){
+		letter = Name[i];
+		nextnode = malloc(sizeof(struct link));
+		nextnode->name = letter;
+		if (start == NULL) {
+			nextnode->next = NULL;
+			start = nextnode;
+		}
+		else {
+			temp = start;
+			while (temp->next != NULL){
+				temp = temp->next;
+			}
+			temp->next = nextnode;
+			nextnode->next = NULL;
+		}
+		i++;
+	}
+	
+	temp = start;
+	while (temp != NULL){
+		printf("%d\n", temp->name);
+		temp = temp->next;
+	}
+	
+	return 0;
+}
